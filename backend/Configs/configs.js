ca432cd78d2a020352937ee5c68b8d62b55f9110
Sugan-s <30893152+Sugan-s@users.noreply.github.com>
2018-09-06 23:11:17 +0530

    port no changed

--- backend/Configs/configs.js
+++ backend/Configs/configs.js
@@ -1,5 +1,5 @@
 module.exports.connectionString = 'mongodb://IMS_SLIIT:ims123@ds133762.mlab.com:33762/ims_matrix';
-module.exports.port = '3001';
+module.exports.port = '3000';
 module.exports.salt = "@FFS1234@";
 module.exports.merchant = "10010";
 module.exports.key = "password1234";
